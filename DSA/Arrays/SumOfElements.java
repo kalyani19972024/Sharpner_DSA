6a20ebe3514f3e26044088956651f9f59fdc84da
Sahu, Sunil Kumar <sunil.sahu@ge.com>
2025-03-29 16:21:20 +0530

    Sum of Elements in an Array
    Sum of Elements in an Array

---
+++ DSA/Arrays/SumOfElements.java
@@ -0,0 +1,12 @@
+class Sum {
+    public static void main(String[] args) {
+        int[] arr = [1, 2, 3, 4, 5];
+        int sum = 0;
+        for (int num : arr
+        ) {
+            sum = sum + num;
+
+        }
+        System.out.println("Sum::" + sum)
+    }
+}
